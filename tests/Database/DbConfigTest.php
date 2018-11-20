82cda45eab124b1384ea908b955026a5ab700efb
tempcke <t.bitbucket@logikostech.net>
2018-11-20 12:36:35 -0600

    now have 100% test coverage on DbConfig

--- tests/Database/DbConfigTest.php
+++ tests/Database/DbConfigTest.php
@@ -4,20 +4,38 @@ namespace LogikosTest\Database;
 
 use Logikos\Database\DbConfig;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
 
 class DbConfigTest extends TestCase {
 
-  public function testSqlite() {
+  public function testSqliteDefaults() {
     $c = DbConfig::sqlite();
-    $this->assertTrue($c->isSqlite());
-    $this->assertEquals('sqlite', $c->driver);
-    $this->assertEquals(':memory:', $c->location);
-    $this->assertTrue($c->isValid());
+    Assert::assertTrue($c->isSqlite());
+    Assert::assertEquals('sqlite', $c->driver);
+    Assert::assertEquals(':memory:', $c->location);
+    Assert::assertEquals('sqlite::memory:',$c->dsn());
+    Assert::assertTrue($c->isValid());
+  }
+
+  public function testSqlite() {
+    $c = DbConfig::sqlite('loc');
+    Assert::assertTrue($c->isSqlite());
+    Assert::assertEquals('sqlite', $c->driver);
+    Assert::assertEquals('loc', $c->location);
+    Assert::assertEquals('sqlite:loc',$c->dsn());
+    Assert::assertTrue($c->isValid());
   }
 
   public function testMysql() {
-    $c = new DbConfig();
-    $this->assertFalse($c->isSqlite());
+    $c = DbConfig::mysql('h','n','u','p',123);
+    Assert::assertEquals('h', $c->host);
+    Assert::assertEquals('n', $c->name);
+    Assert::assertEquals('u', $c->user);
+    Assert::assertEquals('p', $c->pass);
+    Assert::assertEquals(123, $c->port);
+    Assert::assertEquals('mysql', $c->driver);
+    Assert::assertEquals('mysql:host=h;port=123;dbname=n',$c->dsn());
+    Assert::assertFalse($c->isSqlite());
   }
 
   public function testDsn() {
@@ -26,5 +44,6 @@ class DbConfigTest extends TestCase {
 
   public function testIsSqlite() {
     $this->markTestSkipped();
+
   }
 }
